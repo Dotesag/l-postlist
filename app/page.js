89cb87dbdd7697768905737651d6242927f5f713
марина марина <tima.d.sor@yandex.ru>
2025-04-15 22:03:45 +0300

    abc

--- app/page.js
+++ app/page.js
@@ -1,95 +1,94 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
+import "./style.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [plist, setList] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setList(data));
+  }, []);
+
+  const addPost = () => {
+    if (!title.trim() || !body.trim()) return;
+
+    const newPost = {
+      id: Date.now(),
+      title,
+      body,
+      userId: 33,
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(newPost),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    setList([newPost, ...plist]);
+    setBody("");
+    setTitle("");
+  };
+
+  return (
+    <html>
+      <body>
+        <h1>Список постов</h1>
+        <div className="cont">
+          <div className="panel">
+            <h1>Панель управления</h1>
+
+            <div className="newpost">
+              <p>Создать пост</p>
+              <input
+                placeholder="Название"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              ></input>
+              <br />
+              <input
+                placeholder="Текст"
+                value={body}
+                onChange={(e) => setBody(e.target.value)}
+              ></input>
+              <br />
+              <button onClick={addPost}>Создать</button>
+            </div>
+          </div>
+
+          <div className="list">
+            {plist.length > 0 ? (
+              <CreateList plist={plist} setList={setList} />
+            ) : (
+              <h1>Загрузка...</h1>
+            )}
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </body>
+    </html>
   );
+}
+
+function CreateList({ plist, setList }) {
+  const deletePost = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "DELETE",
+    });
+    setList(plist.filter((post) => post.id != id));
+  };
+
+  const listItems = plist.map((post) => (
+    <li className="post">
+      <p>{post.title}</p>
+      <p style={{ color: "#737373" }}>{post.body}</p>
+      <button onClick={() => deletePost(post.id)}>Удалить</button>
+    </li>
+  ));
+  return listItems;
 }
